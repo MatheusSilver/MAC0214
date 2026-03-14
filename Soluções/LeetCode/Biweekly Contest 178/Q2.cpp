8ebf8954b51b11c0993820508d74bab19dde86df
Matheus Silver <matheus.feitosa16@outlook.com>
2026-03-14 12:57:04 -0300

    Chore: Add Biweekly Contest 178

---
+++ Soluções/LeetCode/Biweekly Contest 178/Q2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long gcdSum(vector<int>& nums) {
+        vector<int> prefixGcd(nums.size());
+
+        int mxi = nums[0];
+        for (int i = 0; i < nums.size();i++){
+            mxi = max(mxi, nums[i]);
+            prefixGcd[i] = gcd(nums[i], mxi);
+        }
+
+        sort(prefixGcd.begin(), prefixGcd.end());
+
+        long long answer = 0;
+        int leftIndex = 0;
+        int rightIndex = nums.size()-1;
+        while (rightIndex > leftIndex) {
+            answer += gcd(prefixGcd[leftIndex++], prefixGcd[rightIndex--]);
+        }
+
+        return answer;
+    }
+};©leetcode
